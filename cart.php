c65ac9833655890b691504e8e45b69c02e8a5821
Noel <noles1100@gmail.com>
2024-01-05 20:17:34 +0530

    cart updated commence attack

--- cart.php
+++ cart.php
@@ -14,97 +14,221 @@ include 'config.php';
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
     <link rel="stylesheet" href="fonts/icomoon/style.css">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
     <link rel="stylesheet" href="css/jquery-ui.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
     <link rel="stylesheet" href="css/aos.css">
 
     <link rel="stylesheet" href="css/style.css">
 
     <style>
-    /* CSS styles for the heading */
     #i {
-        font-family: Arial, sans-serif;
-        font-size: 36px;
-        color: black;
-        text-align: center;
-        text-shadow: 2px 2px 4px rgb(228, 227, 227);
-        margin-bottom: 10px;
+      font-family: Arial, sans-serif;
+      font-size: 36px;
+      color: black;
+      /* Blue color, you can change this to your preferred color */
+      text-align: center;
+      text-shadow: 2px 2px 4px rgb(228, 227, 227);
+      /* Add a subtle text shadow */
     }
-    </style>
+  </style>
+  <style>
+    #p {
+      width: 300px;
+      margin: 0 10px;
+      box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.1);
+      transition: 0.2s;
+      height: 400px;
+    }
 
-    <style>
+    #p img {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      width: 50%;
+      border-radius: 4px;
+    }
+
+    #p:hover {
+      transform: scale(1.20);
+      z-index: 1.5;
+    }
+
+    #l {
+      color: black;
+    }
+
+    #x {
+      box-shadow: 0 0 20px 2px rgba(0, 0, 0, .1);
+    }
+  </style>
+  <style>
     #d {
-        border: 1px solid #ccc;
-        /* Optional: Add a border for visualization */
-        padding: 20px;
-        /* Add padding to create space inside the container */
+      border: 1px solid #ccc;
+      /* Optional: Add a border for visualization */
+      padding: 20px;
+      /* Add padding to create space inside the container */
     }
 
     h2 {
-        margin-top: 10px;
-        /* Add margin to the top of the heading to create space */
-    }
-    </style>
-
-    <style>
-    .white-link {
-        color: white;
-        /* Add any other desired styles */
+      margin-top: 10px;
+      /* Add margin to the top of the heading to create space */
     }
 
-    .white-link:hover {
-        color: white;
+    #z {
+      background-color: rgba(60, 85, 140, 0.7);
+      /* Semi-transparent black background */
+      color: white;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.2);
+      /* White box shadow for a cool effect */
+      text-align: center;
+      align-items: center;
+    }
+
+
+    #z1 {
+      background-color: rgba(60, 85, 140, 0.7);
+      /* Semi-transparent black background */
+      color: white;
+      padding: 20px;
+      border-radius: 10px;
+      box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.2);
+      /* White box shadow for a cool effect */
+      text-align: center;
+      align-items: center;
+    }
+
+    #z1:hover {
+      transform: scale(1.10);
+      /* Scale up on hover */
+      transition: transform 0.3s ease-in-out;
+    }
+
+    #z1 {
+      transition: transform 0.3s ease-in-out;
+      /* Apply the same transition without scaling */
+    }
+
+    /* Add shade effect to all images inside the container */
+    #cat .block-2-item::before {
+      content: "";
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background: rgba(0, 0, 0, 0.2);
+      /* Adjust the opacity to control the shade */
+      transition: background 0.3s ease;
+    }
+
+    /* Change the shade effect on hover */
+    #cat .block-2-item:hover::before {
+      background: rgba(0, 0, 0, 0);
+      /* Change to transparent on hover */
+    }
+  </style>
+  <style>
+    .custom-button {
+      padding: 10px 20px;
+      margin-right: 10px;
+      display: inline-flex;
+      align-items: center;
+      background-color: aliceblue;
+      outline: none;
+      border: none;
+
+    }
+
+    .custom-button .icon {
+      margin-right: 10px;
+    }
+
+    .site-navbar {
+      background-color: aliceblue;
     }
 
 
     .site-logo {
-        position: relative;
-        overflow: hidden;
+      position: relative;
+      overflow: hidden;
     }
 
     .js-logo-clone {
-        display: inline-block;
-        font-size: 2rem;
-        position: relative;
-        overflow: hidden;
-        color: transparent;
-        transition: width 1s ease, opacity 1s ease;
-        width: 0;
-        white-space: nowrap;
-        animation: revealText 2s ease forwards;
+      display: inline-block;
+      font-size: 2rem;
+      position: relative;
+      overflow: hidden;
+      color: transparent;
+      transition: width 1s ease, opacity 1s ease;
+      width: 0;
+      white-space: nowrap;
+      animation: revealText 2s ease forwards;
     }
 
     @keyframes revealText {
-        0% {
-            width: 0;
-        }
+      0% {
+        width: 0;
+      }
 
-        100% {
-            width: 80%;
-        }
+      100% {
+        width: 80%;
+      }
     }
 
+    /* Style the image and text */
     .logo-image {
-        max-width: 15%;
-        /* Make the image responsive */
-        height: auto;
-        /* Automatically adjust the image height */
-        display: inline-block;
-        vertical-align: middle;
-        /* Align the image vertically with the text */
+      max-width: 15%;
+      /* Make the image responsive */
+      height: auto;
+      /* Automatically adjust the image height */
+      display: inline-block;
+      vertical-align: middle;
+      /* Align the image vertically with the text */
     }
-    </style>
+  </style>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <style>
+    /* Styles for the comment box */
+    .comment-box {
+      position: absolute;
+      background-color: rgba(0, 0, 255, 0.1);
+      /* Transparent blue background */
+      border: 1px solid #ffffff;
+      padding: 5px 10px;
+      border-radius: 5px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+      opacity: 1;
+      /* Initially visible */
+      transition: opacity 1s;
+      z-index: 1;
+    }
+
+    /* Styles for the icon with the comment box */
+    #login-icon {
+      position: relative;
+      color: blue;
+    }
+
+    /* Styles for the comment text */
+    #login-comment p {
+      margin: 0;
+    }
+
+    /* When the comment box is hidden */
+    #login-icon.clicked #login-comment {
+      opacity: 0;
+    }
+  </style>
 
     <style>
     /* Add this CSS code to your stylesheet */
@@ -124,13 +248,15 @@ include 'config.php';
 </head>
 
 <body>
-    <div class="site-wrap">
+    
+<div class="site-wrap">
         <header class="site-navbar" role="banner">
             <div class="site-navbar-top">
-                <div class="container">
+                <div class="">
                     <div class="row align-items-center">
 
-                        <div class="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left">
+                        <div class="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left"
+                            style="padding-left: 50px;">
                             <form action="" class="site-block-top-search">
                                 <span class="icon icon-search2"></span>
                                 <input type="text" class="form-control border-0" placeholder="Search">
@@ -146,19 +272,36 @@ include 'config.php';
                             </div>
                         </div>
 
-                        <div class="col-6 col-md-4 order-3 order-md-3 text-right">
+                        <div class="col-6 col-md-4 order-3 order-md-3 text-right" style="padding-right: 50px;">
                             <div class="site-top-icons">
                                 <ul>
-                                    <li><a href="signin.html"><span class="icon icon-person"></span></a></li>
-                                    <li><a href="#"><span class="icon icon-heart-o"></span></a></li>
+                                    <?php
+    if (isset($_SESSION['username'])) {
+        // User is logged in as a regular user
+        echo '<li><a href="userprofile.php" class="btn btn-sm custom-button">
+                    <span class="icon"><i class="fas fa-user"></i></span>' . $_SESSION['username'] .
+            '</a></li>';
+        $_SESSION['login'] = true;
+    } elseif (isset($_SESSION['seller_id'])) {
+        // User is logged in as a seller
+        echo '<li><a href="sellerprofile.php" class="btn btn-sm custom-button">
+                    <span class="icon"><i class="fas fa-user"></i></span>' . $_SESSION['firstname'] .
+            '</a></li>';
+    } else {
+        // User is not logged in
+        echo '<li><a href="seller.php" class="btn btn-sm custom-button">
+                    <span class="icon"><i class="fas fa-solid fa-store"></i></span>Become a Seller
+                </a></li>';
+        echo '<li><a href="signin.php" class="btn btn-sm custom-button">
+                    <span class="icon"><i class="fas fa-sign-in-alt"></i></span> Login
+                </a></li>';
+    }
+    ?>
                                     <li>
-                                        <a href="cart.html" class="site-cart active" style="color: blue;">
-                                            <span class="icon icon-shopping_cart"></span>
+                                        <a href="cart.php" class="site-cart btn btn-sm custom-button">
+                                            <span class="icon icon-shopping_cart"><i class="fas"></i></span> Cart
                                             <span class="count">2</span>
                                         </a>
-                                    </li>
-                                    <li class="d-inline-block d-md-none ml-md-0"><a href="#"
-                                            class="site-menu-toggle js-menu-toggle"><span class="icon-menu"></span></a>
                                     </li>
                                 </ul>
                             </div>
@@ -170,27 +313,19 @@ include 'config.php';
             <nav class="site-navigation text-right text-md-center" role="navigation">
                 <div class="container">
                     <ul class="site-menu js-clone-nav d-none d-md-block">
-                        <li class="has-children">
+                        <li class="">
                             <a href="index.php">Home</a>
-                            <ul class="dropdown">
-                                <li><a href="index.html#fre">Featured Products</a></li>
-                                <li><a href="index.html#com">Combo Offers</a></li>
-                                <li><a href="bigsale.html">Big Sale!</a></li>
-                            </ul>
                         </li>
-                        <li class="has-children">
+                        <li class="">
                             <a href="about.html">About</a>
-                            <ul class="dropdown">
-                                <li><a href="about.html#TheTeam">The Team</a></li>
-                                <li><a href="#i">Help</a></li>
-                            </ul>
                         </li>
-                        <li><a href="shop.html">Shop</a></li>
+                        <li class=""><a href="shop.php">Shop</a></li>
                         <li><a href="contact.html">Contact</a></li>
                     </ul>
                 </div>
             </nav>
         </header>
+
 
         <div class="bg-light py-3">
             <div class="container">
@@ -291,7 +426,7 @@ if (isset($_SESSION['username'])) {
 
                             <div class="col-md-6">
                                 <button class="btn btn-primary btn-sm btn-block"><a href="shop.html"
-                                        class="white-link">Continue Shopping</a></button>
+                                        class="white-link" style="color: white;">Continue Shopping</a></button>
                             </div>
                         </div>
                         <div class="row">
@@ -349,77 +484,75 @@ if (isset($_SESSION['username'])) {
     </div>
 
     <footer class="site-footer border-top">
-        <div>
-            <div class="row justify-content-center">
-                <div class="col-md-7 site-section-heading text-center pt-4">
-                    <h2 id="i">Help</h2>
-                </div>
-            </div>
+      <div>
+        <div class="row justify-content-center">
+          <div class="col-md-7 site-section-heading text-center pt-4">
+            <h2 id="i">Help</h2>
+          </div>
         </div>
-        <div class="container" id="d">
+      </div>
+      <div class="container" id="d">
+        <div class="row">
+          <div class="col-lg-6 mb-5 mb-lg-0">
             <div class="row">
-                <div class="col-lg-6 mb-5 mb-lg-0">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h3 class="footer-heading mb-4">Navigations</h3>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Sell online</a></li>
-                                <li><a href="#">Features</a></li>
-                                <li><a href="#">Shopping cart</a></li>
-                                <li><a href="#">Store builder</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Mobile commerce</a></li>
-                                <li><a href="#">Dropshipping</a></li>
-                                <li><a href="#">Website development</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Point of sale</a></li>
-                                <li><a href="#">Hardware</a></li>
-                                <li><a href="#">Software</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
-                    <h3 class="footer-heading mb-4">Promo</h3>
-                    <a href="#" class="block-6">
-                        <img src="images/hero_1.jpg" alt="Image placeholder" class="img-fluid rounded mb-4">
-                        <h3 class="font-weight-light  mb-0">Finding Your Perfect Shoes</h3>
-                        <p>Promo from nuary 15 &mdash; 25, 2019</p>
-                    </a>
-                </div>
-                <div class="col-md-6 col-lg-3">
-                    <div class="block-5 mb-5">
-                        <h3 class="footer-heading mb-4">Contact Info</h3>
-                        <ul class="list-unstyled">
-                            <li class="address">203 Fake St. Mountain View, San Francisco, California, USA</li>
-                            <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
-                            <li class="email">emailaddress@domain.com</li>
-                        </ul>
-                    </div>
-
-                    <div class="block-7">
-                        <form action="#" method="post">
-                            <label for="email_subscribe" class="footer-heading">Subscribe</label>
-                            <div class="form-group">
-                                <input type="text" class="form-control py-4" id="email_subscribe" placeholder="Email">
-                                <input type="submit" class="btn btn-sm btn-primary" value="Send">
-                            </div>
-                        </form>
-                    </div>
-                </div>
+              <div class="col-md-12">
+                <h3 class="footer-heading mb-4">Navigations</h3>
+              </div>
+              <div class="col-md-6 col-lg-4">
+                <ul class="list-unstyled">
+                  <li><a href="demo.html">Sell online</a></li>
+                  <li><a href="about.html">Features</a></li>
+                  <li><a href="cart.html">Shopping cart</a></li>
+                  <li><a href="shop.php">Shop</a></li>
+                </ul>
+              </div>
+              <div class="col-md-6 col-lg-4">
+                <ul class="list-unstyled">
+                  <li><a href="shop.php">Categories</a></li>
+                  <li><a href="about.html">Shipping Details</a></li>
+                  <li><a href="profile.php">Profile</a></li>
+                </ul>
+              </div>
+              <div class="col-md-6 col-lg-4">
+                <ul class="list-unstyled">
+                  <li><a href="seller.php">How to become a Seller?</a></li>
+                  <li><a href="#">Manage Order</a></li>
+                </ul>
+              </div>
             </div>
-            <div class="row pt-5 mt-5 text-center">
-
+          </div>
+          <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+            <h3 class="footer-heading mb-4"></h3>
+            <a href="#" class="block-6">
+              <!-- Adjust the max-width value to reduce the image size further -->
+              <img src="images/kart.jpg" alt="Image placeholder" class="img-fluid rounded mb-4" style="width: 100%; max-width: 100px; height: auto;">
+              <h3 class="font-weight-light  mb-0">Finding Your Perfect Gadgets</h3>
+              <p>Make Living a Habit</p>
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="block-5 mb-5">
+              <h3 class="footer-heading mb-4">Contact Info</h3>
+              <ul class="list-unstyled">
+                <li class="address">203 St. Mountain View, San Francisco, California, USA</li>
+                <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
+                <li class="email">emailaddress@domain.com</li>
+              </ul>
             </div>
+  
+            <div class="block-7">
+              <form action="#" method="post">
+                <label for="email_subscribe" class="footer-heading"></label>
+                <div class="form-group">
+                  <input type="text" class="form-control py-4" id="email_subscribe" placeholder="Email">
+                  <input type="submit" class="btn btn-sm btn-primary" value="Send">
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
+
+      </div>
     </footer>
     </div>
 
